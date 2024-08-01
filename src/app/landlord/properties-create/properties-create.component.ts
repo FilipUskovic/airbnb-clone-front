7c3c0f36a0122e2162c6b59de1b4c3adb6f1e2f9
Filip Uskovic <filip.uskovic@outlook.com>
2024-08-01 15:46:21 +0200

    dodao picture step i upload za slike

--- src/app/landlord/properties-create/properties-create.component.ts
+++ src/app/landlord/properties-create/properties-create.component.ts
@@ -14,6 +14,7 @@ import {CategoryStepComponent} from "./step/category-step/category-step.componen
 import {FooterStepComponent} from "../../shared/footer-step/footer-step.component";
 import {LocationStepComponent} from "./step/location-step/location-step.component";
 import {InfoStepComponent} from "./step/info-step/info-step.component";
+import {PictureStepComponent} from "./step/picture-step/picture-step.component";
 
 @Component({
   selector: 'app-properties-create',
@@ -22,7 +23,8 @@ import {InfoStepComponent} from "./step/info-step/info-step.component";
     CategoryStepComponent,
     FooterStepComponent,
     LocationStepComponent,
-    InfoStepComponent
+    InfoStepComponent,
+    PictureStepComponent
   ],
   templateUrl: './properties-create.component.html',
   styleUrl: './properties-create.component.scss'
---
+++ src/app/landlord/properties-create/step/picture-step/picture-step.component.ts
@@ -0,0 +1,70 @@
+import {Component, EventEmitter, input, Output} from '@angular/core';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {InputTextModule} from "primeng/inputtext";
+import {ButtonModule} from "primeng/button";
+import {NewListingPicture} from "../../../model/picture.model";
+
+@Component({
+  selector: 'app-picture-step',
+  standalone: true,
+  imports: [
+    FontAwesomeModule,
+    InputTextModule,
+    ButtonModule
+  ],
+  templateUrl: './picture-step.component.html',
+  styleUrl: './picture-step.component.scss'
+})
+export class PictureStepComponent {
+
+  pictures = input.required<Array<NewListingPicture>>();
+
+  @Output()
+  pictureChange = new EventEmitter<Array<NewListingPicture>>();
+  @Output()
+  stepValidityChange = new EventEmitter<boolean>();
+
+  onUploadNewPicture(target: EventTarget | null){
+    const pictureFileList = this.extractFileFromTaregt(target);
+    if (pictureFileList !== null){
+      for (let i = 0; i < pictureFileList.length; i++){
+        const picture = pictureFileList.item(i);
+        if(picture !== null){
+          const displayPicture: NewListingPicture = {
+            file: picture,
+            // base 64
+            urlDisplay: URL.createObjectURL(picture)
+          }
+          // push to the singal
+          this.pictures().push(displayPicture);
+        }
+      }
+      // notifiy parent component
+      this.pictureChange.emit(this.pictures());
+      this.validatePictures();
+    }
+  }
+
+  private extractFileFromTaregt(target: EventTarget | null) {
+    const htmlInputTarget = target as HTMLInputElement;
+    if (target === null || htmlInputTarget.files === null){
+      return null;
+    }
+    return htmlInputTarget.files;
+  }
+
+  private validatePictures() {
+    if (this.pictures.length >= 5) {
+      this.stepValidityChange.emit(true);
+    } else {
+      this.stepValidityChange.emit(false);
+    }
+  }
+
+  onTrashPicture(pictureToDelete: NewListingPicture) {
+    const indexToDelete =
+      this.pictures().findIndex(picture => picture.file.name === pictureToDelete.file.name);
+      this.pictures().splice(indexToDelete, 1);
+      this.validatePictures();
+  }
+}
